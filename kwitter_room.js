8d9ff3acc9650cdf9d5c756e59b48f02a8f7def3
Donut-Icing <80368570+Donut-Icing@users.noreply.github.com>
2021-10-31 11:20:02 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+const firebaseConfig = {apiKey: "AIzaSyDR3-2EUSZ_aZ1f-nvT4hUzBUdxXj-HuRA",
+  authDomain: "kwitterproject-3a1cd.firebaseapp.com",
+  databaseURL: "https://kwitterproject-3a1cd-default-rtdb.firebaseio.com",
+  projectId: "kwitterproject-3a1cd",
+  storageBucket: "kwitterproject-3a1cd.appspot.com",
+  messagingSenderId: "862966753305",
+  appId: "1:862966753305:web:6f5361a8ed6e04bc1a6288",
+  measurementId: "G-N43J0XCTBN"}
